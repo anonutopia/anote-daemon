499258f5dfc86cdb0363672d6653870ef5e5c7e0
Pragmatic <metaceo01@gmail.com>
2018-08-01 04:43:00 +0200

    unique

--- models.go
+++ models.go
@@ -12,7 +12,7 @@ type User struct {
 	gorm.Model
 	Nickname                string `sql:"size:255"`
 	Email                   string `sql:"size:255"`
-	Address                 string `sql:"size:255"`
+	Address                 string `sql:"size:255;unique_index"`
 	Referral                string `sql:"size:255"`
 	BitcoinAddr             string `sql:"size:255"`
 	BitcoinBalanceNew       int    `sql:"DEFAULT:0"`
@@ -32,5 +32,5 @@ type User struct {
 	ReferralProfitEthTotal  uint64
 	ReferralProfitWavTotal  uint64
 	ReferralProfitBtcTotal  uint64
-	ReceivedFreeAnote       bool `sql:"DEFAULT:false"`
+	TelegramId              int `sql:"DEFAULT:0;unique_index"`
 }
